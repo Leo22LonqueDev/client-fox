53c0f497e40057ebc2c02625ff69033f13c0544e
Leo22LonqueDev <leonardoarrudalonque@outlook.com>
2024-01-18 20:39:50 -0300

    Adicionando next.js

---
+++ src/app/pages/home/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <>
+            <h1>Duas telas funcionando, ta bom ou quer mais</h1>
+        </>
+    )
+
+}
---
+++ src/app/pages/login/index.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+    return (
+        <>
+            <h1>Hello Pissotinho, funciona</h1>
+        </>
+    )
+
+}
