e8f7ef64ea27b41d026bf67a24d1abb4f0525220
Leo22LonqueDev <leonardoarrudalonque@outlook.com>
2024-01-22 22:51:30 -0300

    Update leonardo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,20 @@
 "use client";
 
-import { Box, Button, Container, TextField } from "@mui/material";
+import { Box, Button, Container, Paper, TextField } from "@mui/material";
+import logoFox from "@/imgs/logoFox.svg"
 
 export default function Home() {
     return (
         <>
             <Container>
-                <Box sx={{ display: "flex", alignItems: "center", flexDirection: "column", alignContent: "center" }}>
-                    <h2>Auto-escola FOX</h2>
-                    <TextField type="email" label="E-mail" variant="standard" />
+                <Box component={Paper} sx={{ width: '30%', display: "flex", alignItems: "center", flexDirection: "column", justifyContent: "center" }}>
+                    <img src={logoFox} alt="Logo Fox" srcSet="logo Fox" />
+                    <TextField type="email" label="E-mail" variant="standard" sx={{ width: '300px' }} />
                     <br />
-
-
-                    <TextField type="password" label="Senha" variant="standard" />
-
+                    <TextField type="password" label="Senha" variant="standard" sx={{ width: '300px' }} />
                     <br />
-
-                    <Button>
-                        Login
-                    </Button>
+                    <Button type='submit' variant='contained'>Login</Button>
+                    <br />
                 </Box>
             </Container>
         </>
