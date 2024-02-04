0c062317a31a98068969357492a15afc3fafe31a
Leo22LonqueDev <leonardoarrudalonque@outlook.com>
2024-02-04 10:33:22 -0300

    Update client

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import AppRoutes from './AppRoutes';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
