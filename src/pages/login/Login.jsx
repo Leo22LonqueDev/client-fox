d6f5ae60202c52f923e96ec9db87f5ccf3a087bf
Leo22LonqueDev <leonardoarrudalonque@outlook.com>
2024-02-04 12:20:10 -0300

    Update client

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,17 @@
 import { Box, Button, Container, TextField } from "@mui/material"
 import { orange } from "@mui/material/colors"
+import logo from '../../imgs/logo.png'
 
 const Login = () => {
     return (
         <>
-        <h1>Funciona</h1>
-            <Container maxWidth sx={{ bgcolor: orange[600] }}>
-                <Box>
-                    <TextField type='email' label='E-mail' />
-                    <TextField type='password' label='Senha' />
-                    <Button type='submit'>LOGIN</Button>
+            <Container maxWidth sx={{ bgcolor: orange[600], height: '100vh' }}>
+                <Box maxWidth={400} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', bgcolor: orange[50], padding: '10px' }}>
+                    <img src={logo} alt='LogoFox' />
+                    <TextField type='email' label='E-mail' variant='standard' />
+                    <TextField type='password' label='Senha' variant='standard' />
+                    <br />
+                    <Button type='submit' variant='contained'>LOGIN</Button>
                 </Box>
             </Container>
         </>
