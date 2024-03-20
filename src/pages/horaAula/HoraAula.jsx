817a2c229acf6de36cdb06aad95f94de581cfab3
Leo22LonqueDev <leonardoarrudalonque@outlook.com>
2024-03-20 20:18:52 -0300

    Update leonardo

--- src/pages/horaAula/HoraAula.jsx
+++ src/pages/horaAula/HoraAula.jsx
@@ -1,33 +1,40 @@
-import { Box, Container, Paper, Typography, ThemeProvider, TableCell, TableContainer, Table, TableHead, TableRow, TableBody, Button, TextField, IconButton } from "@mui/material"
+import { Box, Container, Paper, Typography, ThemeProvider, TableCell, TableContainer, Table, TableHead, TableRow, TableBody, Button, TextField, IconButton, DialogTitle, DialogContent, Dialog, DialogContentText, InputLabel, Select, FormControl, DialogActions, Snackbar, Alert, Autocomplete } from "@mui/material"
 import SidebarSee from "../../components/Sidebar/Sidebar"
 import * as React from 'react'
 import { orange } from "@mui/material/colors"
 import { useState } from "react"
 import { Search } from "@mui/icons-material"
-
-
-
-
-
+import { MenuItem } from "react-pro-sidebar"
 
 const HoraAula = () => {
 
     const [open, setOpen] = useState(false)
     const [openHoraAula, setOpenHoraAula] = useState(false)
+    const [severity, setSeverity] = useState('')
+    const [message, setMessage] = useState('')
 
     const handleClose = async () => {
         setOpen(false)
         setOpenHoraAula(false)
     }
 
+    const getCarros = async () => {
+        try {
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     const openCriarFinanceiro = async () => {
         setOpenHoraAula(true)
     }
+
     return (
         <>
             <SidebarSee>
                 <Container>
-                <Box
+                    <Box
                         sx={{
                             display: 'flex',
                             alignItems: 'center',
@@ -59,8 +66,73 @@ const HoraAula = () => {
                         >
                             Hora Aula
                         </Typography>
-                        <Button type='Button' variant='contained' color='info' onClick={openHoraAula} >Adicionar hora Aula</Button>
+                        <Button type='Button' variant='contained' color='info' onClick={openCriarFinanceiro} >Adicionar hora Aula</Button>
                     </Box>
+                    <Dialog
+                        open={openHoraAula}
+                        onClose={handleClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                    >
+                        <DialogTitle id="alert-dialog-title">
+                            {"Informe os dados da Hora-Aula"}
+                        </DialogTitle>
+                        <DialogContent>
+                            <DialogContentText id="alert-dialog-description" sx={{ display: 'flex', flexDirection: 'column' }}>
+                                <Autocomplete
+                                disablePortal
+                                id='Veículo'
+                                />
+                                <TextField type='text' label='Quantidade' sx={{ mt: 2 }}
+                                    InputProps={{
+                                        style: {
+                                            borderRadius: '10px',
+                                        }
+                                    }} />
+                                <TextField type='text' label='Valor' sx={{ mt: 2 }}
+                                    InputProps={{
+                                        style: {
+                                            borderRadius: '10px',
+                                        }
+                                    }} />
+                                <FormControl margin='normal' >
+                                    <InputLabel id="demo-simple-select-label">Tipo de Pagamento</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={''}
+                                        label='Tipo de depagamento'
+
+                                    >
+                                        <MenuItem value={'pix'} >PIX</MenuItem>
+                                        <MenuItem value={'dinheiro'}>DINHEIRO</MenuItem>
+                                        <MenuItem value={'boleto'}>BOLETO</MenuItem>
+                                        <MenuItem value={'cheque'}>CHEQUE</MenuItem>
+                                        <MenuItem value={'cartao Crédito'}>CARTÃO CRÉDITO</MenuItem>
+                                        <MenuItem value={'cartao Débito'}>CARTÃO DÉBITO</MenuItem>
+                                    </Select>
+                                </FormControl>
+                                <TextField type='date' label='Data Realização Pagamento' sx={{ mt: 2 }}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    InputProps={{
+                                        style: {
+                                            borderRadius: '10px',
+                                        }
+                                    }} />
+                            </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose} color='error'>Fechar</Button>
+                            <Button onClick={handleClose} color='success' autoFocus>Criar</Button>
+                        </DialogActions>
+                    </Dialog>
+                    <Snackbar open={open} autoHideDuration={5000} onClose={() => { setOpen(false) }}>
+                        <Alert severity={severity} variant='filled'>
+                            {message}
+                        </Alert>
+                    </Snackbar>
                     <Box
                         sx={{ mt: 10 }}
                     >
@@ -92,7 +164,7 @@ const HoraAula = () => {
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    
+
                                 </TableBody>
                             </Table>
                         </TableContainer>
