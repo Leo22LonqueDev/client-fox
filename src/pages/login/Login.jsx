6ebdc611f0977b4a96486970883e2076071691d3
Leo22LonqueDev <leonardoarrudalonque@outlook.com>
2024-03-31 23:17:28 -0300

    Update leonardo

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,28 +1,53 @@
-import { Box, Button, Container, TextField } from "@mui/material"
+import { Box, Button, CircularProgress, Container, TextField } from "@mui/material"
 import { orange } from "@mui/material/colors"
 import logo from '../../imgs/logo.png'
 import { useState } from "react"
+import axios from "axios"
 
 const Login = () => {
 
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
+    const [loading, setLoading] = useState(false)
 
     const handleSubmit = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        try {
+            const result = await axios.post(`${process.env.REACT_APP_BACKEND}/login`, {
+                email,
+                senha
+            })
+            console.log(result);
+            if (result.status === 200) {
+                const values = result.data
+                localStorage.setItem('values', values)
 
+                window.location.replace('/')
+            }
+        } catch (error) {
+            console.log(error);
+            setLoading(false)
+        }
     }
 
     return (
         <>
             <Container maxWidth sx={{ bgcolor: orange[600], height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', bgcolor: orange[50], padding: '40px', borderRadius: '15px', height: '400px' }}>
-                    <img src={logo} alt='LogoFox' />
-                    <TextField type='email' label='E-mail' onChange={(e) => { setEmail(e.target.value) }} value={email} variant='standard' sx={{ width: '300px' }} />
-                    <br />
-                    <TextField type='password' label='Senha' onChange={(e) => { setSenha(e.target.value) }} value={senha} variant='standard' sx={{ width: '300px' }} />
-                    <br />
-                    <Button type='submit' variant='contained' onClick={handleSubmit} >LOGIN</Button>
-                </Box>
+                {
+                    loading ? (
+                        <CircularProgress />
+                    ) : (
+                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', bgcolor: orange[50], padding: '40px', borderRadius: '15px', height: '400px' }}>
+                            <img src={logo} alt='LogoFox' />
+                            <TextField type='email' label='E-mail' onChange={(e) => { setEmail(e.target.value) }} value={email} variant='standard' sx={{ width: '300px' }} />
+                            <br />
+                            <TextField type='password' label='Senha' onChange={(e) => { setSenha(e.target.value) }} value={senha} variant='standard' sx={{ width: '300px' }} />
+                            <br />
+                            <Button type='submit' variant='contained' onClick={handleSubmit} >LOGIN</Button>
+                        </Box>
+                    )
+                }
             </Container>
         </>
     )
